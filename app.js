8fae2d0e65c8ce12b0d971ce4adbf9b92dcd6c47
Felipe Acosta <facosta0787@gmail.com>
2022-03-15 18:17:47 -0500

    v1.4 - Se puede agregar tareas, marcar y desmarcarlas

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ let $tasksCounter = document.querySelector('#contador-tareas-pendientes')
 let $newTaskForm = document.querySelector('.new-task-form')
 let $resetFormButton = document.querySelector('.clear-button')
 let $tasksList = document.querySelector('.tasks-list')
+let $newTask = document.querySelector('.caja-de-text')
+
+$newTaskForm.addEventListener('submit', addTask) // addTaskt(event)
 
 let todos = [
     {
         id: 1646783778271,
-        description: 'Practigar mucho JavaScript',
+        description: 'Practicar mucho JavaScript',
         isDone: false
     },
     {
         id: 1646783778275,
         description: 'Aprender funcionesdel Array en JS',
-        isDone: false
+        isDone: true
     },
     {
         id: 1646783778279,
@@ -23,18 +26,55 @@ let todos = [
     },
 ];
 
+function addTask(event) {
+    event.preventDefault()
+
+    if($newTask.value === '') {
+        return // Detenemos la ejecucion de la funcion
+    }
+
+    let newTask = {
+        id: new Date().getTime(),
+        description: $newTask.value, // $newTask: referencia al input de HTML
+        isDone: false
+    }
+
+    todos.push(newTask)
+    renderTodos()
+    $newTask.value = ''
+}
+
+function checkTask(posicion){
+    todos[posicion].isDone = todos[posicion].isDone === true ? false : true;
+    renderTodos()
+}
+
+function renderPendingTasks() {
+    const pendingTasksArray = todos.filter(function(task) {
+        const noEstaTerminada = task.isDone === false
+        return noEstaTerminada
+    })
+
+    const counterPendingTasks = pendingTasksArray.length
+    $tasksCounter.innerHTML = `${counterPendingTasks} ${counterPendingTasks > 1 ? 'Tareas pendientes' : 'Tarea pendiente'}`
+}
+
 function renderTodos() {
+    renderPendingTasks()
     $tasksList.innerHTML = ''
     
     // renderedTask es un ARRAY que retorna todos.map
-    let renderedTasks = todos.map(function(tarea) {
+    // ARRAY.map Siempre devuelve un array 
+    let resultTasks = todos.map(function(task) {
         return `
         <li class="task-list-item">
             <button class="button-list check-task-button">
-                <i class="fa-regular fa-circle-check"></i>
+                <i
+                    class="fa-regular fa-circle-check ${task.isDone === true ? 'fa-circle-check-done' : ''}"
+                ></i>
             </button>
             <span class="task-description">
-                ${tarea.description}
+                ${task.description}
             </span>
             <button type="button" class="button-list remove-task-button">
                 <i class="fa-regular fa-trash-can"></i>
@@ -42,7 +82,16 @@ function renderTodos() {
         </li> 
         `
     })
-    $tasksList.innerHTML = renderedTasks.join('') 
+    // Resultado de las tareas unidas
+    $tasksList.innerHTML = resultTasks.join('')
+
+
+    const $checkButtons = document.querySelectorAll('.check-task-button')
+    $checkButtons.forEach(function($checkButton, posicion) {
+        $checkButton.addEventListener('click', function(){
+            checkTask(posicion)
+        })
+    })
 }
 
 renderTodos()
